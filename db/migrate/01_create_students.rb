a23335ae811fc942007b8cb146637c9b3a9018ea
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-21 14:50:02 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -4,7 +4,7 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   #Define change method and use Active Record create_table, tbale should have a :name TEXT , string 
   #run rake db:migrate after change method 
   
-  def create_table
+  def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
     id INTEGER PRIMARY KEY,
