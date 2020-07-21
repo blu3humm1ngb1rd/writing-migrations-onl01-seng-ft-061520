4c35a0297632713f43fd05da2ecb708cb964e46b
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-21 14:43:27 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,10 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  
+  #create table with ActiveRecord 
+  #Define change method and use Active Record create_table, tbale should have a :name TEXT , string 
+  #run rake db:migrate after change method 
+  
+  def create_table
+    
+  end 
 end
